a259a9588b78eeef169a372dc63471a0ee3f9d89
Jacob Rohde <68932646+jarohde@users.noreply.github.com>
2022-06-09 16:37:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="rnt",
-    version="0.0.12",
+    version="0.0.13",
     author="Jacob Rohde",
     author_email="jarohde1@gmail.com",
     description="A simple tool for generating and analyzing Reddit networks.",
