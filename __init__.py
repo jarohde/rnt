4d5afca1f728dd3a98e25cad6d1c5aa8f766a125
Jacob Rohde <68932646+jarohde@users.noreply.github.com>
2023-04-01 09:57:24 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,11 @@
+# See https://github.com/jarohde/rnt for more information.
+
+"""
+rnt: The Reddit Network Toolkit for Python.
+"""
+
+__version__ = "0.1.6"
+__author__ = "Jacob Rohde"
+__license__ = "MIT"
+
 from .rnt import *
